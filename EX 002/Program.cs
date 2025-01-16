e1c54baefa85e5277a335cc4ed1b6c7c42f90128
carolline <carolline.rassis@senacsp.edu.br>
2025-01-16 19:27:24 -0300

    Adicionar arquivos de projeto.

---
+++ EX 002/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EX_002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 2;
+            float num = 2.35f;
+            char letra = 'c';
+            String nome = "CAROL";
+
+            Console.WriteLine("O número é {0}.",numero);
+            Console.WriteLine("O valor é R${0}.",num);
+            Console.WriteLine("A letra é {0}.",letra);
+            Console.WriteLine("O seu nome é {0}.",nome);
+
+            Console.WriteLine("O número é {0}, o valor é R$ {1}, a letras é {2} o seu nome é {3}.", numero, num, letra, nome);
+        }
+    }
+}
---
+++ Ex_005/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, num4;
+            Console.WriteLine("Digite a nota");
+
+            num1= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("digite a outra nota:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            
+            num4= (num1 + num2)/ 2;
+
+            Console.WriteLine("É soma é {0}", media);
+
+        }
+    }
+}

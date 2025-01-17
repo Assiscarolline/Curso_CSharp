e67bae0d5ddde9e34b6a2303516c130f17d2637b
carolline <carolline.rassis@senacsp.edu.br>
2025-01-16 21:23:06 -0300

    Adicionando recuperação ao programa.

--- EX_007/Program.cs
+++ EX_007/Program.cs
@@ -4,17 +4,25 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 70;
+            double nota_final = 40;
             string resultado;
 
             if (nota_final >= 60)
             {
                 resultado = "aprovado";
             }
+            else if (nota_final >= 40)
+            {
+
+                resultado = "recuperação";
+            }
+
             else 
+
             {
                 resultado = "reprovado";
             }
+
 
 
             Console.WriteLine("O aluno foi {0}.", resultado);

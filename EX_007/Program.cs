bdfb6f633c979b8cca173c57c8dbe4addff19b75
carolline <carolline.rassis@senacsp.edu.br>
2025-01-16 22:09:51 -0300

    Insirido calculo para várias notas

--- EX_007/Program.cs
+++ EX_007/Program.cs
@@ -4,28 +4,48 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 40;
+            double nota_final, n1, n2, n3, n4;
             string resultado;
+
+            Console.Write("digite a primeira nota do aluno: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a segunda nota do aluno: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a terceira nota do aluno: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a quartanota do aluno: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
 
             if (nota_final >= 60)
             {
-                resultado = "aprovado";
+                if (nota_final >= 90)
+                {
+                    resultado = "aprovado com louvor";
+                }
+                      
+                else
+                {
+                    resultado = "aprovado";
+                }
+                    
+                
             }
-            else if (nota_final >= 40)
-            {
-
-                resultado = "recuperação";
-            }
-
             else 
-
             {
-                resultado = "reprovado";
+                if (nota_final >= 40);
+                {
+                    resultado = "reprovado";
+                }
             }
 
 
 
-            Console.WriteLine("O aluno foi {0}.", resultado);
+            Console.WriteLine("Nota do aluno: {0} - resultado : {1}.",nota_final, resultado);
         }
     }
 }

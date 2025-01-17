57038a75ec1fadcb6a21ffd8e243dd8672d104c3
carolline <carolline.rassis@senacsp.edu.br>
2025-01-16 21:06:48 -0300

    Atualização do exercicio.

--- EX_007/Program.cs
+++ EX_007/Program.cs
@@ -3,8 +3,21 @@
     internal class Program
     {
         static void Main(string[] args)
-        
-            Console.WriteLine("Hello, World!");
+        {
+            double nota_final = 70;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "aprovado";
+            }
+            else 
+            {
+                resultado = "reprovado";
+            }
+
+
+            Console.WriteLine("O aluno foi {0}.", resultado);
         }
     }
 }

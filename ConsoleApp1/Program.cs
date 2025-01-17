d5a4eaa3dbc7f66c756c8d5dca18eaa6066d033f
carolline <carolline.rassis@senacsp.edu.br>
2025-01-16 22:25:34 -0300

    Texte de programa que leia par ou impar (não concluido).

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int par_ou_impar;
+
+            string resultado;
+
+            Console.WriteLine("digite o numero");
+            par_ou_impar = 0;
+
+            if (par_ou_impar ==2) ;
+            
+
+        }
+    }
+}

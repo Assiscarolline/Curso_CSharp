16cf2567a8f6bc2ec77c4153286fe7707dfa762e
carolline <carolline.rassis@senacsp.edu.br>
2025-01-21 22:26:38 -0300

    Primeiro teste deo empréstimo

---
+++ EX_0012/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EX_0012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double casa, salario, tempo;
+
+             Console.WriteLine("Digite o seu valor da casa: ");
+            casa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o seu salário: ");
+            salario =Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o tempo: ");
+            tempo = Convert.ToDouble(Console.ReadLine());
+
+            
+            
+
+        }
+    }
+}

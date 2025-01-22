e0bab8e464a4688e353d5d9c8e05cc035283a4ce
carolline <carolline.rassis@senacsp.edu.br>
2025-01-21 21:56:08 -0300

    Teste de programa para calculo de salário

---
+++ Ex_0011/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex_0011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, salario_final;
+
+            Console.WriteLine("Digite o seu salário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                salario_final = salario * 0.05 + salario;
+                Console.WriteLine("O salário final é {0}.", salario_final);
+            }
+            else if (salario > 1250)
+            {
+                salario_final = salario * 0.10 + salario;
+                Console.WriteLine("O salário final é {0}.", salario_final); 
+             
+            }
+            else 
+            {
+                salario_final = salario * 0.15 + salario;
+                Console.WriteLine("O salário final é {0}.", salario_final);
+            }
+
+
+               
+
+        }
+    }
+}

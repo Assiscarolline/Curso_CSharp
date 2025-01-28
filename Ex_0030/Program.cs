d95e5402d70472c3da7274493ef7d360da1c21d2
carolline <carolline.rassis@senacsp.edu.br>
2025-01-28 19:45:31 -0300

    Programa de contador de senha

---
+++ Ex_0030/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex_0030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+            while(num < 5)
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+
+            }
+            do
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+
+            } while(num < 5);
+        }
+    }
+}
---
+++ senha/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123"; //   senha correta
+            string senhaDigitada; // senha digitada pelo usuário
+            int tentativas = 0; // contador de tentativas
+
+            // Loop para solicitar a senha do usúario
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas ++; // Incrementa o contador de tentativas
+                //Verifica se a senha digitada é diferente da senha correta 
+
+                if (senhaDigitada != senha && tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta!");
+                    Console.WriteLine("Você ainda tem:" + (3 - tentativas) + "tentativa(s)");
+                    Console.ReadKey();
+                } 
+                else if (senhaDigitada != senha && tentativas == 3)
+
+                {
+                    Console.WriteLine("Senha incorreta! Seu bandidinho!");
+                    return; // serve para parar o loop
+
+                }
+            }while(senhaDigitada != senha);
+             
+            Console.Clear() ;
+            Console.WriteLine("senha correta! - Tentativas:" + tentativas);
+            
+            
+        }
+    }
+}

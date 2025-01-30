a5184382c75381b32ec1081bfe02a77504fb4741
carolline <carolline.rassis@senacsp.edu.br>
2025-01-30 20:55:33 -0300

    teste de métodos

---
+++ Frutas/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Frutas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = { "Pessêgo", "Abacate", "Jabuticaba", "Kiwi", "Laranja", "Mexerica", "Banana", "Mangá", "maçã", "Uva" };
+            double[] reais = { 10, 5, 10, 20, 10, 10, 6, 10, 12, 15 };
+            
+
+
+
+
+
+        }
+
+        
+    }
+}
---
+++ Métodos/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args) // main é o método principal
+        {
+            saudacao("Carolline");  //Olá Carol, seja bem vinda
+            soma(10, 20);
+        }
+
+        static void saudacao (string nome) // saudacao é um método
+        {
+            Console.WriteLine($"Olá {nome}, seja bem-vinda!");
+        }
+        static void soma(int a, int b)
+        {
+            int resultado = a + b; 
+            Console.WriteLine( $"a SOMA DE {a} + {b} É IGUAL A {resultado}");
+            Console.WriteLine("A soma de {0}+ {1} é igual a {2}.", a, b, resultado);
+            Console.WriteLine("A soma de "+ a +" + " + b + " é igual a " + resultado + ".");
+
+            // ambos são o mesmo resultado só mudam a formula
+        }
+
+    }
+}

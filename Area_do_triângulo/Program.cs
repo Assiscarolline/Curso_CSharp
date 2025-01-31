56c6d72aae00e313f4106eb8a2275ac51bd4f941
carolline <carolline.rassis@senacsp.edu.br>
2025-01-30 21:39:21 -0300

    Area do triangulo

---
+++ Area_do_triângulo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Area_do_triângulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************************");
+            Console.WriteLine("Digite a aréa do triagulo:");
+            Console.WriteLine("**************************");
+
+            Console.Write("Digite a base: ");
+            double baseA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite a altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Area(baseA, altura);
+        }
+        static void Area(double baseA, double altura )
+        {
+            double A = altura * baseA / 2;
+            Console.WriteLine($"A aréa do triângulo {altura} x {baseA} = {A}");
+        }
+
+        
+    }
+}
---
+++ Calculo aréa/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculo_aréa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================");
+            Console.WriteLine("Controle de terrenos");
+            Console.WriteLine("====================");
+
+            Console.Write("Digite a Largura do terreno(m)");
+            double largura =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite o comprimento do terreno(m)");
+            double comprimento = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            areaterreno(largura, comprimento);
+        }
+        static void areaterreno(double l, double c)
+        {
+            double a = l * c;
+            Console.WriteLine($"A área do terreno de {l}m x {c}m é de {a:F2}m².");
+        }
+           
+    }
+
+}

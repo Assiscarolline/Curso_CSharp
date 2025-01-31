2647c4cfa2d5635cbf582ec5fd111616fe477592
carolline <carolline.rassis@senacsp.edu.br>
2025-01-30 22:19:54 -0300

    CONTADOR

---
+++ Contador/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Contador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            contador(1, 10, 1);
+            contador(10,0,2);
+            Escrever("Agora é sua vez de personalizar a contagem!");
+            Console.Write("Digite o inicio: ");
+            int ini =Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o fim: ");
+            int fim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite os passos: ");
+            int pas = Convert.ToInt32(Console.ReadLine());
+            
+
+        }
+        static void contador(int inicio, int fim, int passo)
+        {
+            Escrever($"Contagem de {inicio} até {fim} de {passo} em {passo}");
+            Thread.Sleep(2000);
+
+            if (inicio < fim) //Condição falsa chama o else
+            {
+                int cont = inicio;
+                while (cont <= fim)
+                {
+                    Console.WriteLine($"{cont} ");
+                    Thread.Sleep(500);
+                    cont += passo;
+             
+                }
+                Console.WriteLine("FIM!");
+            }   
+            else
+            {
+
+                int cont = inicio;
+                while (cont >= fim)
+                {
+                    Console.WriteLine($"{cont} ");
+                    Thread.Sleep(500);
+                    cont -= passo;
+
+                }
+            }
+            Console.WriteLine("FIM");
+
+        
+        }
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('=', tam);
+            string textoCentralizado = texto.PadLeft(tam - 2).PadRight(tam - 2);
+
+            Console.WriteLine(linha);
+            Console.WriteLine(textoCentralizado);
+            Console.WriteLine(linha);
+
+        }
+         //QUANDO O PASSO É ZERO
+    }
+}
